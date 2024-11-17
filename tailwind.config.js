5ee8f8267f98b242df9a05327d32bf1adb3b2feb
013wutever <nikolazervoudakis@gmail.com>
2024-11-17 02:50:46 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#fdf2f8',
+          100: '#fce7f3',
+          200: '#fbcfe8',
+          300: '#f9a8d4',
+          400: '#f472b6',
+          500: '#ec4899',
+          600: '#db2777',
+          700: '#be185d',
+          800: '#9d174d',
+          900: '#831843',
+        },
+        glassmorphic: {
+          light: 'rgba(255, 255, 255, 0.1)',
+          medium: 'rgba(255, 255, 255, 0.15)',
+          strong: 'rgba(255, 255, 255, 0.2)',
+        }
+      },
+      backdropBlur: {
+        xs: '2px',
+      },
+      screens: {
+        'xs': '375px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+};
